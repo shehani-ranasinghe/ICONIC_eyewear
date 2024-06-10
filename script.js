3a6c9298113102cd7392955a1468a72c56ab522d
shehani <152892060+shehani-ranasinghe@users.noreply.github.com>
2024-06-10 15:39:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
+
