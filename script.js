fd1e2cc9e116f3bce7fdd94d76db267f57fa51db
shehani <152892060+shehani-ranasinghe@users.noreply.github.com>
2024-07-10 17:36:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let currentIndex = 0;
+
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.slider img');
+const totalImages = images.length;
+
+document.querySelector('.next-handle').addEventListener('click', () => {
+    if (currentIndex < totalImages - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    updateSlider();
+});
+
+document.querySelector('.prev-handle').addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = totalImages - 1;
+    }
+    updateSlider();
+});
+
+function updateSlider() {
+    const offset = -currentIndex * 100;
+    slider.style.transform = `translateX(${offset}%)`;
+}
 
